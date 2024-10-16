73bcbdca934f1b2e7489f23bddfe813733cea341
Shivaansh Kapoor <shivaanshkapoor02@gmail.com>
2024-10-16 15:56:50 -0700

    deposit test

--- test/TestPredepositLocker.t.sol
+++ test/TestPredepositLocker.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 // Import the PredepositLocker contract and its dependencies
 import { PredepositLocker, RecipeMarketHubBase, ERC20 } from "src/PredepositLocker.sol";
-import { RecipeMarketHubTestBase, RecipeMarketHubBase, RewardStyle } from "test/utils/RecipeMarketHubTestBase.sol";
+import { RecipeMarketHubTestBase, RecipeMarketHubBase, RewardStyle, Points } from "test/utils/RecipeMarketHubTestBase.sol";
 import { IStargate } from "src/interfaces/IStargate.sol";
 import { FixedPointMathLib } from "@solmate/utils/FixedPointMathLib.sol";
 import { WeirollWalletHelper } from "test/utils/WeirollWalletHelper.sol";
@@ -17,6 +17,9 @@ contract TestPredepositLocker is RecipeMarketHubTestBase {
     address AP_ADDRESS;
     address FRONTEND_FEE_RECIPIENT;
 
+    ERC20[] public predepositTokens;
+    IStargate[] public stargates;
+
     PredepositLocker predepositLocker;
     WeirollWalletHelper walletHelper;
 
@@ -24,12 +27,17 @@ contract TestPredepositLocker is RecipeMarketHubTestBase {
     bytes32 marketHash;
 
     function setUp() external {
+        walletHelper = new WeirollWalletHelper();
+
         uint256 protocolFee = 0.01e18; // 1% protocol fee
         uint256 minimumFrontendFee = 0.001e18; // 0.1% minimum frontend fee
         setUpRecipeMarketHubTests(protocolFee, minimumFrontendFee);
 
-        // predepositLocker = new PredepositLocker(owner);
-        walletHelper = new WeirollWalletHelper();
+        IP_ADDRESS = ALICE_ADDRESS;
+        AP_ADDRESS = BOB_ADDRESS;
+        FRONTEND_FEE_RECIPIENT = CHARLIE_ADDRESS;
+
+        predepositLocker = new PredepositLocker(OWNER_ADDRESS, 0, address(0), predepositTokens, stargates, recipeMarketHub);
 
         RecipeMarketHubBase.Recipe memory DEPOSIT_RECIPE =
             _buildDepositRecipe(PredepositLocker.deposit.selector, address(walletHelper), address(mockLiquidityToken), address(predepositLocker));
@@ -37,22 +45,109 @@ contract TestPredepositLocker is RecipeMarketHubTestBase {
 
         frontendFee = recipeMarketHub.minimumFrontendFee();
         marketHash = recipeMarketHub.createMarket(address(mockLiquidityToken), 30 days, frontendFee, DEPOSIT_RECIPE, WITHDRAWAL_RECIPE, RewardStyle.Forfeitable);
-
-        uint256 offerAmount = 100_000e18; // Offer amount requested
-        uint256 fillAmount = 1000e18; // Fill amount
-
-        IP_ADDRESS = ALICE_ADDRESS;
-        AP_ADDRESS = BOB_ADDRESS;
-        FRONTEND_FEE_RECIPIENT = CHARLIE_ADDRESS;
     }
 
-    function test_depositing(uint256 offerAmount, uint256 fillAmount) external {
-        vm.assume(fillAmount <= offerAmount && fillAmount > 0);
+    function test_depositIntoPredepositLocker(uint256 offerAmount, uint256 fillAmount) external {
+        vm.assume(fillAmount <= offerAmount && fillAmount > 1e18);
+        vm.assume(offerAmount < type(uint96).max && offerAmount > 1e18);
+
+        assertEq(mockLiquidityToken.balanceOf(address(predepositLocker)), 0);
+
+        // Mint liquidity tokens to the AP to fill the offer
+        mockLiquidityToken.mint(AP_ADDRESS, fillAmount);
+        vm.startPrank(AP_ADDRESS);
+        mockLiquidityToken.approve(address(recipeMarketHub), fillAmount);
+        vm.stopPrank();
+
+        // Create a fillable IP offer for points
+        (bytes32 offerHash, Points points) = createIPOffer_WithPoints(marketHash, offerAmount, IP_ADDRESS);
+
+        vm.expectEmit(false, true, false, false, address(mockLiquidityToken));
+        emit ERC20.Transfer(address(0), address(predepositLocker), fillAmount);
+
+        vm.expectEmit(true, false, false, false, address(predepositLocker));
+        emit PredepositLocker.UserDeposited(marketHash, address(0), fillAmount);
+
+        // Record the logs to capture Transfer events to get Weiroll wallet address
+        vm.recordLogs();
+        // AP Fills the offer (no funding vault)
+        vm.startPrank(AP_ADDRESS);
+        recipeMarketHub.fillIPOffers(offerHash, fillAmount, address(0), FRONTEND_FEE_RECIPIENT);
+        vm.stopPrank();
+
+        // Extract the Weiroll wallet address (the 'to' address from the Transfer event - third event in logs)
+        address weirollWallet = address(uint160(uint256(vm.getRecordedLogs()[0].topics[2])));
+
+        assertEq(mockLiquidityToken.balanceOf(weirollWallet), 0);
+        assertEq(mockLiquidityToken.balanceOf(address(predepositLocker)), fillAmount);
+    }
+
+    // Get fill amount using Weiroll Helper -> Approve fill amount -> Call Deposit
+    function _buildDepositRecipe(
+        bytes4 _depositSelector,
+        address _helper,
+        address _tokenAddress,
+        address _predepositLocker
+    )
+        internal
+        pure
+        returns (RecipeMarketHubBase.Recipe memory)
+    {
+        bytes32[] memory commands = new bytes32[](3);
+        bytes[] memory state = new bytes[](2);
+
+        state[0] = abi.encode(_predepositLocker);
+
+        // GET FILL AMOUNT
+
+        // DELEGATECALL
+        uint8 f = uint8(0x00);
+
+        // Input list: No arguments (END_OF_ARGS = 0xff)
+        bytes6 inputData = hex"ffffffffffff";
+
+        // Output specifier (fixed length return value stored at index 0 of the output array)
+        // 0xff ignores the output if any
+        uint8 o = 0x01;
+
+        // Encode args and add command to RecipeMarketHubBase.Recipe
+        commands[0] = (bytes32(abi.encodePacked(WeirollWalletHelper.amount.selector, f, inputData, o, _helper)));
+
+        // APPROVE Predeposit Locker to spend tokens
+
+        // CALL
+        f = uint8(0x01);
+
+        // Input list: Args at state index 0 (address) and args at state index 1 (fill amount)
+        inputData = hex"0001ffffffff";
+
+        // Output specifier (fixed length return value stored at index 0 of the output array)
+        // 0xff ignores the output if any
+        o = 0xff;
+
+        // Encode args and add command to RecipeMarketHubBase.Recipe
+        commands[1] = (bytes32(abi.encodePacked(ERC20.approve.selector, f, inputData, o, _tokenAddress)));
+
+        // CALL DEPOSIT() in Predeposit Locker
+        f = uint8(0x01);
+
+        // Input list: No arguments (END_OF_ARGS = 0xff)
+        inputData = hex"ffffffffffff";
+
+        // Output specifier (fixed length return value stored at index 0 of the output array)
+        // 0xff ignores the output if any
+        o = uint8(0xff);
+
+        // Encode args and add command to RecipeMarketHubBase.Recipe
+        commands[2] = (bytes32(abi.encodePacked(_depositSelector, f, inputData, o, _predepositLocker)));
+
+        return RecipeMarketHubBase.Recipe(commands, state);
     }
 
     function _buildWithdrawalRecipe(bytes4 _withdrawalSelector, address _predepositLocker) internal pure returns (RecipeMarketHubBase.Recipe memory) {
         bytes32[] memory commands = new bytes32[](1);
         bytes[] memory state = new bytes[](0);
+
         // Flags:
         // DELEGATECALL (calltype = 0x00)
         // CALL (calltype = 0x01)
@@ -69,67 +164,6 @@ contract TestPredepositLocker is RecipeMarketHubTestBase {
 
         // Encode args and add command to RecipeMarketHubBase.Recipe
         commands[0] = (bytes32(abi.encodePacked(_withdrawalSelector, f, inputData, o, _predepositLocker)));
-
-        return RecipeMarketHubBase.Recipe(commands, state);
-    }
-
-    // Get fill amount using Weiroll Helper -> Approve fill amount -> Call Deposit
-    function _buildDepositRecipe(
-        bytes4 _depositSelector,
-        address _helper,
-        address _tokenAddress,
-        address _predepositLocker
-    )
-        internal
-        pure
-        returns (RecipeMarketHubBase.Recipe memory)
-    {
-        bytes32[] memory commands = new bytes32[](3);
-        bytes[] memory state = new bytes[](1);
-
-        state[0] = abi.encodePacked(_predepositLocker);
-
-        // GET FILL AMOUNT
-
-        // DELEGATECALL
-        uint8 f = uint8(0x00);
-
-        // Input list: No arguments (END_OF_ARGS = 0xff)
-        bytes6 inputData = hex"ffffffffffff";
-
-        // Output specifier (fixed length return value stored at index 0 of the output array)
-        // 0xff ignores the output if any
-        uint8 o = 0x01;
-
-        // Encode args and add command to RecipeMarketHubBase.Recipe
-        commands[0] = (bytes32(abi.encodePacked(_depositSelector, f, inputData, o, _helper)));
-
-        // APPROVE
-        // CALL
-        f = uint8(0x01);
-
-        // Input list: No arguments (END_OF_ARGS = 0xff)
-        inputData = hex"00ffffffffff";
-
-        // Output specifier (fixed length return value stored at index 0 of the output array)
-        // 0xff ignores the output if any
-        o = 0x00;
-
-        // Encode args and add command to RecipeMarketHubBase.Recipe
-        commands[1] = (bytes32(abi.encodePacked(bytes4(keccak256("function approve(address,uint256)")), f, inputData, o, _tokenAddress)));
-
-        // CALL DEPOSIT() in Predeposit Locker
-        f = uint8(0x01);
-
-        // Input list: No arguments (END_OF_ARGS = 0xff)
-        inputData = hex"00ffffffffff";
-
-        // Output specifier (fixed length return value stored at index 0 of the output array)
-        // 0xff ignores the output if any
-        o = uint8(0xff);
-
-        // Encode args and add command to RecipeMarketHubBase.Recipe
-        commands[2] = (bytes32(abi.encodePacked(_depositSelector, f, inputData, o, _predepositLocker)));
 
         return RecipeMarketHubBase.Recipe(commands, state);
     }
